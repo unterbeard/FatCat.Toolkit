2b0494faa1f91447189cd4db2e1970b6aa021f56
David Basarab <dbasarab617@gmail.com>
2022-04-17 20:14:21 -0400

    Added Console.Exception logging

--- src/OneOff/Program.cs
+++ src/OneOff/Program.cs
@@ -1,14 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using FatCat.Toolkit.Console;
+﻿using FatCat.Toolkit.Console;
 using FatCat.Toolkit.Events;
 
-ConsoleLog.LogCallerInformation = false;
+// ConsoleLog.LogCallerInformation = false;
 
 ConsoleLog.WriteBlue("Hello WORLD!!!!!!");
 
 var consoleUtilities = new ConsoleUtilities(new WaitEvent());
 
+try { new WillDoBadStuff().DoBadStuff(); }
+catch (Exception e) { ConsoleLog.WriteException(e); }
+
 consoleUtilities.WaitForExit();
 
 ConsoleLog.WriteMagenta("This is after the wait for exit");
+
+public class WillDoBadStuff
+{
+	public void DoBadStuff() { ActuallyThrow(); }
+
+	private void ActuallyThrow() => throw new Exception("I did something really bad like Bart Simpson");
+}
--- src/ToolKit/Console/ConsoleLog.cs
+++ src/ToolKit/Console/ConsoleLog.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using FatCat.Toolkit.Enumerations;
 
 namespace FatCat.Toolkit.Console;
 
@@ -68,6 +69,31 @@ public static class ConsoleLog
 										[CallerFilePath] string sourceFilePath = "",
 										[CallerLineNumber] int sourceLineNumber = 0) => Write(ConsoleColor.DarkYellow, message, memberName, sourceFilePath, sourceLineNumber);
 
+	public static void WriteEmptyLine() => WriteLineWithColor(ConsoleColor.Black, string.Empty);
+
+	public static void WriteException(Exception exception,
+									[CallerMemberName] string memberName = "",
+									[CallerFilePath] string sourceFilePath = "",
+									[CallerLineNumber] int sourceLineNumber = 0)
+	{
+		var level = 0;
+
+		var currentException = exception;
+
+		WriteEquals(ConsoleColor.Red);
+
+		while (currentException != null && level < 5)
+		{
+			level++;
+
+			WriteExceptionForLevel(currentException, level, memberName, sourceFilePath, sourceLineNumber);
+
+			currentException = currentException.InnerException;
+		}
+
+		WriteEquals(ConsoleColor.Red, NewLineLocation.Before);
+	}
+
 	public static void WriteGray(string message,
 								[CallerMemberName] string memberName = "",
 								[CallerFilePath] string sourceFilePath = "",
@@ -98,6 +124,28 @@ public static class ConsoleLog
 									[CallerFilePath] string sourceFilePath = "",
 									[CallerLineNumber] int sourceLineNumber = 0) => Write(ConsoleColor.Yellow, message, memberName, sourceFilePath, sourceLineNumber);
 
+	private static string? GetPrintableStackTrace(string spaces, Exception exception) => string.IsNullOrEmpty(exception.StackTrace) ? null : exception.StackTrace.Replace(Environment.NewLine, $"{spaces}{Environment.NewLine}                ");
+
+	private static void WriteEquals(ConsoleColor color, NewLineLocation lineLocation = NewLineLocation.None)
+	{
+		if (lineLocation.IsFlagSet(NewLineLocation.Before)) WriteEmptyLine();
+
+		WriteLineWithColor(color, new string('=', 125));
+
+		if (lineLocation.IsFlagSet(NewLineLocation.After)) WriteEmptyLine();
+	}
+
+	private static void WriteExceptionForLevel(Exception exception, int level, [CallerMemberName] string memberName = "",
+												[CallerFilePath] string sourceFilePath = "",
+												[CallerLineNumber] int sourceLineNumber = 0)
+	{
+		var spaces = new string(' ', level * 4);
+
+		WriteEmptyLine();
+		WriteRed($"{spaces}Error     : {exception.Message}", memberName, sourceFilePath, sourceLineNumber);
+		WriteRed($"{spaces}StackTrace: {GetPrintableStackTrace(spaces, exception)}", memberName, sourceFilePath, sourceLineNumber);
+	}
+
 	private static void WriteLineWithColor(ConsoleColor color, string message)
 	{
 		lock (lockObj)
@@ -110,5 +158,13 @@ public static class ConsoleLog
 
 			System.Console.ForegroundColor = oldColor;
 		}
+	}
+
+	private enum NewLineLocation
+	{
+		None = 0,
+		Before = 1,
+		After = 2,
+		Both = Before | After
 	}
 }
