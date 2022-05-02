fed85cc0009413130da9bdeac92efdf14e88cc11
David Basarab <dbasarab617@gmail.com>
2022-05-02 14:29:37 -0400

    got basic sample going

--- src/OneOff/Program.cs
+++ src/OneOff/Program.cs
@@ -8,26 +8,63 @@ namespace OneOff;
 
 public static class Program
 {
-	public static void Main(params string[] args)
+	private static SpikeServer server;
+
+	public static async Task Main(params string[] args)
 	{
+		const int tcpPort = 62222;
+
 		ConsoleLog.WriteBlue("Going to implement a TCP Client/Server");
 
 		var consoleUtilities = new ConsoleUtilities(new ManualWaitEvent());
 
-		var server = new SpikeServer(IPAddress.Any, 62222);
+		if (args.Any(i => i.Contains("s")))
+		{
+			server = new SpikeServer(IPAddress.Any, tcpPort);
 
-		server.Start();
+			server.Start();
+		}
+		else
+		{
+			var client = new SpikeTcpClient(new IPEndPoint(IPAddress.Parse("127.0.0.1"), tcpPort));
+
+			await client.Send("This is my first test message");
+
+			consoleUtilities.Exit();
+		}
 
 		consoleUtilities.WaitForExit();
 
-		server.Dispose();
+		server?.Dispose();
+	}
+}
+
+public class SpikeTcpClient : IDisposable
+{
+	private readonly IPEndPoint endPoint;
+
+	public SpikeTcpClient(IPEndPoint endPoint) => this.endPoint = endPoint;
+
+	public void Dispose() { }
+
+	public async Task Send(string message)
+	{
+		using var client = new TcpClient();
+
+		await client.ConnectAsync(endPoint);
+
+		await using var networkStream = client.GetStream();
+
+		var messageBytes = Encoding.UTF8.GetBytes(message);
+
+		await networkStream.WriteAsync(messageBytes);
 	}
 }
 
 public class SpikeServer : IDisposable
 {
-	private readonly TcpListener listener;
 	private readonly CancellationTokenSource cancelSource;
+	private readonly TcpListener listener;
 
 	public SpikeServer(IPAddress ipAddress, ushort port)
 	{
@@ -36,18 +73,16 @@ public class SpikeServer : IDisposable
 		cancelSource = new CancellationTokenSource();
 	}
 
-	public void Close() => listener.Stop();
-
 	public void Dispose()
 	{
 		cancelSource.Dispose();
-		Close();
+		listener.Stop();
 	}
 
 	public void Start()
 	{
 		listener.Start();
-		
+
 		ConsoleLog.WriteCyan($"Listening on <{listener.LocalEndpoint}>");
 
 		listener.BeginAcceptTcpClient(TcpClientConnected, listener);
@@ -59,7 +94,7 @@ public class SpikeServer : IDisposable
 
 		var client = listener.EndAcceptTcpClient(ar);
 
-		ConsoleLog.WriteBlue($"Client Connected from {client}");
+		ConsoleLog.WriteBlue($"Client Connected from {client.Client.RemoteEndPoint}");
 		ConsoleLog.WriteBlue(". . . . . . . Connected!");
 
 		syncListener?.BeginAcceptTcpClient(TcpClientConnected, syncListener);
