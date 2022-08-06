4b09ecff86459670ce1310d0505d7c8ea45c0935
David Basarab <dbasarab617@gmail.com>
2022-08-06 11:39:06 -0400

    generator enhancements

--- src/ToolKit/Generator.cs
+++ src/ToolKit/Generator.cs
@@ -1,13 +1,26 @@
 using FatCat.Fakes;
+using MongoDB.Bson;
 
 namespace FatCat.Toolkit;
 
 public interface IGenerator
 {
+	Guid NewGuid();
+
+	ObjectId NewObjectId();
+
 	int NextRandom(int? minNumber = null, int? maxNumber = null);
+
+	string RandomString(string? prefix = null, int? length = null);
 }
 
 public class Generator : IGenerator
 {
+	public Guid NewGuid() => Guid.NewGuid();
+
+	public ObjectId NewObjectId() => ObjectId.GenerateNewId();
+
 	public int NextRandom(int? minNumber = null, int? maxNumber = null) => Faker.RandomInt(minNumber, maxNumber);
+
+	public string RandomString(string? prefix = null, int? length = null) => Faker.RandomString(prefix, length);
 }
