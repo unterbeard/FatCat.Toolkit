743471a142ce56b4dc8a2e5313125d70746ba434
David Basarab <dbasarab617@gmail.com>
2022-09-13 18:56:21 -0400

    created byte utlitlies

---
+++ src/ToolKit/Utilities/ByteUtilities.cs
@@ -0,0 +1,13 @@
+using FatCat.Toolkit.Extensions;
+
+namespace FatCat.Toolkit.Utilities;
+
+public interface IByteUtilities
+{
+	string ToReadableString(byte[] bytes);
+}
+
+public class ByteUtilities : IByteUtilities
+{
+	public string ToReadableString(byte[] bytes) => bytes.ToReadableString();
+}
---
+++ src/ToolKit/Utilities/StringUtilities.cs
@@ -0,0 +1,8 @@
+namespace FatCat.Toolkit.Utilities;
+
+public interface IStringUtilities { }
+
+public class StringUtilities : IStringUtilities
+{
+	
+}
