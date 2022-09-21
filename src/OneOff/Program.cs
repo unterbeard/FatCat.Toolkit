97d7dec152fab07283479b927890cf8e3877b25c
David Basarab <dbasarab617@gmail.com>
2022-09-20 21:12:43 -0400

    playing around with Find

--- src/OneOff/Program.cs
+++ src/OneOff/Program.cs
@@ -1,4 +1,5 @@
-﻿using FatCat.Toolkit.Console;
+﻿using FatCat.Fakes;
+using FatCat.Toolkit.Console;
 using FatCat.Toolkit.Data;
 using FatCat.Toolkit.Data.Lite;
 using Newtonsoft.Json;
@@ -10,18 +11,25 @@ try
 	using var liteRepository = new LiteDbRepository<Customer>(new LiteDbConnection<Customer>(new DataNames()));
 
 	var databaseFullPath = @"C:\TempWorking\ToolkitSpike\LiteDatabaseSpike\ToolkitData.db";
-	
+
 	liteRepository.Connect(databaseFullPath);
 
-	var customer = new Customer
-					{
-						Name = "John Doe",
-						IsActive = true
-					};
+	var customer = Faker.Create<Customer>(afterCreate: i => i.Id = default);
 
 	var createdObject = await liteRepository.Create(customer);
 
 	ConsoleLog.WriteCyan($"Created Object {JsonConvert.SerializeObject(createdObject, Formatting.Indented)}");
+
+	var allObjects = liteRepository.Collection.FindAll()!;
+
+	ConsoleLog.WriteMagenta($"{JsonConvert.SerializeObject(allObjects, Formatting.Indented)}");
+
+	const string nameToFind = "9462bc67a";
+
+	var foundItems = liteRepository.Collection.Find(i => i.Name == nameToFind);
+	
+	ConsoleLog.WriteBlue($"{JsonConvert.SerializeObject(foundItems, Formatting.Indented)}");
+
 }
 catch (Exception ex) { ConsoleLog.WriteException(ex); }
 
