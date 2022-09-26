3980367a40b6cc1fc9d8510c5ad416a50b3fe036
David Basarab <dbasarab617@gmail.com>
2022-09-25 21:41:46 -0400

    created an IThread fake

---
+++ src/ToolKit/Testing/FakeThread.cs
@@ -0,0 +1,12 @@
+﻿namespace FatCat.Toolkit.Testing;
+
+public class FakeThread : IThread
+{
+	public void Run(Action action) { action(); }
+
+	public void Run(Func<Task> action) { action().Wait(); }
+
+	public Task Sleep(TimeSpan sleepTime) => Task.CompletedTask;
+
+	public Task Sleep(TimeSpan sleepTime, CancellationToken token) => Task.CompletedTask;
+}
