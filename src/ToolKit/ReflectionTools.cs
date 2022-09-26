cb4ac4c063e6109579b221bbffb45f0508cdbe64
David Basarab <dbasarab617@gmail.com>
2022-09-25 20:19:31 -0400

    added FindAttributeOnType

--- src/ToolKit/ReflectionTools.cs
+++ src/ToolKit/ReflectionTools.cs
@@ -5,18 +5,22 @@ namespace FatCat.Toolkit;
 
 public interface IReflectionTools
 {
-	List<Type> FindTypesImplementing<T>(List<Assembly> assemblies);
+	TAttribute? FindAttributeOnType<TAttribute>(Type type) where TAttribute : Attribute;
+
+	List<Type> FindTypesImplementing<TTypeImplementing>(List<Assembly> assemblies);
 }
 
 public class ReflectionTools : IReflectionTools
 {
-	public List<Type> FindTypesImplementing<T>(List<Assembly> assemblies)
+	public TAttribute? FindAttributeOnType<TAttribute>(Type type) where TAttribute : Attribute => type.GetCustomAttribute<TAttribute>();
+
+	public List<Type> FindTypesImplementing<TTypeImplementing>(List<Assembly> assemblies)
 	{
 		var foundTypes = new List<Type>();
 
 		foreach (var assembly in assemblies)
 		{
-			var typesImplementing = assembly.TypesImplementing<T>();
+			var typesImplementing = assembly.TypesImplementing<TTypeImplementing>();
 
 			foundTypes.AddRange(typesImplementing);
 		}
