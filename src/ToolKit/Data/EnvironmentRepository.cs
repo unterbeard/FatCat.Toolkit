c57148f9c0b28f70438c8d64d209f4070ca8b152
David Basarab <dbasarab617@gmail.com>
2022-09-29 19:56:58 -0400

    created env repository

---
+++ src/ToolKit/Data/EnvironmentRepository.cs
@@ -0,0 +1,23 @@
+namespace FatCat.Toolkit.Data;
+
+public interface IEnvironmentRepository
+{
+	string? Get(string name);
+
+	string? Get(string name, EnvironmentVariableTarget target);
+
+	void Set(string name, string? value);
+
+	void Set(string name, string? value, EnvironmentVariableTarget target);
+}
+
+public class EnvironmentRepository : IEnvironmentRepository
+{
+	public string? Get(string name) => Environment.GetEnvironmentVariable(name);
+
+	public string? Get(string name, EnvironmentVariableTarget target) => Environment.GetEnvironmentVariable(name, target);
+
+	public void Set(string name, string? value) => Environment.SetEnvironmentVariable(name, value);
+
+	public void Set(string name, string? value, EnvironmentVariableTarget target) => Environment.SetEnvironmentVariable(name, value, target);
+}
