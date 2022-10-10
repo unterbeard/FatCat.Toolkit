3b0a142d3a54e936fd1ced3978077afc13ab6c0a
David Basarab <dbasarab617@gmail.com>
2022-10-09 21:28:10 -0400

    trying to figure out how to passing a connection string to mongo

---
+++ src/ToolKit/Data/Mongo/GetMongoConnectionString.cs
@@ -0,0 +1,11 @@
+﻿namespace FatCat.Toolkit.Data.Mongo;
+
+public interface IGetMongoConnectionString
+{
+	string? Get();
+}
+
+public class GetMongoConnectionString : IGetMongoConnectionString
+{
+	public string? Get() => null;
+}
