8498062334734544b332701046143b4b85e3afe5
David Basarab <dbasarab617@gmail.com>
2022-10-23 13:39:46 -0400

    starting object tools

---
+++ src/ToolKit/ObjectTools.cs
@@ -0,0 +1,11 @@
+﻿namespace FatCat.Toolkit;
+
+public interface IObjectTools
+{
+	bool IsEquals(object obj1, object obj2);
+}
+
+public class ObjectTools : IObjectTools
+{
+	public bool IsEquals(object obj1, object obj2) => obj1.Equals(obj2);
+}
