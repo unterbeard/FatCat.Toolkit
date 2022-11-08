97aa82223412bc1738b9690cc2c00426ad2b77f2
David Basarab <dbasarab617@gmail.com>
2022-11-07 20:17:19 -0500

    can connect

--- src/OneOff/Program.cs
+++ src/OneOff/Program.cs
@@ -1,35 +1,68 @@
 ﻿using System.Reflection;
+using Autofac;
 using FatCat.Toolkit.Console;
+using FatCat.Toolkit.Injection;
+using FatCat.Toolkit.Threading;
 using FatCat.Toolkit.Web.Api;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace OneOff;
 
 public static class Program
 {
+	private const int WebPort = 14555;
+
 	public static async Task Main(params string[] args)
 	{
 		ConsoleLog.LogCallerInformation = true;
 
 		try
 		{
-			var applicationSettings = new ApplicationSettings
-									{
-										Options = WebApplicationOptions.UseHttps,
-										CertificationLocation = @"C:\DevelopmentCert\DevelopmentCert.pfx",
-										CertificationPassword = "basarab_cert",
-										Port = 14555,
-										ContainerAssemblies = new List<Assembly> { Assembly.GetExecutingAssembly() },
-										CorsUri = new List<Uri> { new("https://localhost:14555") },
-										OnWebApplicationStarted = Started
-									};
+			var isClient = args.Any() && args.Any(i => i.Equals("client", StringComparison.OrdinalIgnoreCase));
 
-			WebApplication.Run(applicationSettings);
+			if (isClient) ConnectClient();
+			else RunServer();
 		}
 		catch (Exception ex) { ConsoleLog.WriteException(ex); }
 	}
 
-	private static void Started()
+	private static void ConnectClient()
 	{
-		ConsoleLog.WriteGreen("Hey the web application has started!!!!!");
+		SystemScope.Initialize(new ContainerBuilder(), ScopeOptions.SetLifetimeScope);
+
+		var consoleUtilities = SystemScope.Container.Resolve<IConsoleUtilities>();
+
+		var thread = SystemScope.Container.Resolve<IThread>();
+		
+		thread.Run(async () =>
+					{
+						var hubUrl = $"https://localhost:{WebPort}/api/events";
+
+						var connection = new HubConnectionBuilder()
+										.WithUrl(hubUrl)
+										.Build();
+
+						await connection.StartAsync();
+					});
+		
+		consoleUtilities.WaitForExit();
 	}
+
+	private static void RunServer()
+	{
+		var applicationSettings = new ApplicationSettings
+								{
+									Options = WebApplicationOptions.UseHttps | WebApplicationOptions.UseSignalR,
+									CertificationLocation = @"C:\DevelopmentCert\DevelopmentCert.pfx",
+									CertificationPassword = "basarab_cert",
+									Port = WebPort,
+									ContainerAssemblies = new List<Assembly> { Assembly.GetExecutingAssembly() },
+									CorsUri = new List<Uri> { new($"https://localhost:{WebPort}") },
+									OnWebApplicationStarted = Started
+								};
+
+		WebApplication.Run(applicationSettings);
+	}
+
+	private static void Started() { ConsoleLog.WriteGreen("Hey the web application has started!!!!!"); }
 }
