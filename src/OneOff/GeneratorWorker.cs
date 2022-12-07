b819e6a95c6c3e459fbbdfb1dd8a0a74793aeb78
David Basarab <dbasarab617@gmail.com>
2022-12-06 22:32:44 -0500

    stuff

---
+++ src/OneOff/GeneratorWorker.cs
@@ -0,0 +1,37 @@
+﻿using FatCat.Toolkit;
+using FatCat.Toolkit.Console;
+
+namespace OneOff;
+
+public class GeneratorWorker
+{
+	private readonly IGenerator generator;
+
+	public GeneratorWorker(IGenerator generator) => this.generator = generator;
+
+	public void DoWork()
+	{
+		var fakeList = new List<int>
+						{
+							0,
+							1,
+							2,
+							3,
+							4,
+							5,
+							6,
+							7,
+							8,
+							9,
+							10,
+							11
+						};
+
+		for (var i = 0; i < 200; i++)
+		{
+			var randomIndex = generator.NextRandom(0, fakeList.Count);
+
+			ConsoleLog.WriteCyan($"RandomIndex: <{randomIndex}> | Value: <{fakeList[randomIndex]}>");
+		}
+	}
+}
