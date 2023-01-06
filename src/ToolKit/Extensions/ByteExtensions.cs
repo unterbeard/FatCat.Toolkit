d34af38d9bcce05618bad9b2df592c3d88436266
David Basarab <dbasarab617@gmail.com>
2023-01-05 22:19:50 -0500

    added some encoded stuff to  byte extensions

--- src/ToolKit/Extensions/ByteExtensions.cs
+++ src/ToolKit/Extensions/ByteExtensions.cs
@@ -4,6 +4,10 @@ namespace FatCat.Toolkit.Extensions;
 
 public static class ByteExtensions
 {
+	public static byte[] FromEncodedString(this string value) => Convert.FromBase64String(value);
+
+	public static string ToEncodedString(this byte[] bytes) => Convert.ToBase64String(bytes);
+
 	public static string ToReadableString(this byte[] bytes)
 	{
 		var finalString = new StringBuilder();
