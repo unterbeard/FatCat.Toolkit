0cd3cda8bd1047f45a29569b8271d3561d27948c
David Basarab <dbasarab617@gmail.com>
2023-01-07 18:45:00 -0500

    added byte tools

---
+++ src/Tests.ToolKit/ByteToolTests.cs
@@ -0,0 +1,25 @@
+﻿using FatCat.Fakes;
+using FatCat.Toolkit;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.FatCat.Toolkit;
+
+public class ByteToolTests
+{
+	[Fact]
+	public void CanConvertToByteArrayAndFrom()
+	{
+		var byteTools = new ByteTools();
+
+		var textToChange = Faker.RandomString();
+
+		var bytes = byteTools.ToBase64Encoded(textToChange);
+
+		var plainText = byteTools.FromBase64Encoded(bytes);
+
+		plainText
+			.Should()
+			.Be(textToChange);
+	}
+}
---
+++ src/ToolKit/ByteTools.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Castle.Core.Internal;
+using FatCat.Toolkit.Extensions;
+
+namespace FatCat.Toolkit;
+
+public interface IByteTools
+{
+	string FromBase64Encoded(byte[] bytes);
+
+	byte[] ToBase64Encoded(string text);
+}
+
+public class ByteTools : IByteTools
+{
+	public string FromBase64Encoded(byte[] bytes)
+	{
+		if (bytes.IsNullOrEmpty()) return string.Empty;
+
+		var base64Text = Convert.ToBase64String(bytes);
+
+		var plainTextBytes = Convert.FromBase64String(base64Text);
+
+		return Encoding.UTF8.GetString(plainTextBytes);
+	}
+
+	public byte[] ToBase64Encoded(string text)
+	{
+		if (text.IsNullOrEmpty()) return Array.Empty<byte>();
+
+		var plainTextBytes = Encoding.UTF8.GetBytes(text);
+
+		var base64Text = Convert.ToBase64String(plainTextBytes);
+
+		return Convert.FromBase64String(base64Text);
+	}
+}
