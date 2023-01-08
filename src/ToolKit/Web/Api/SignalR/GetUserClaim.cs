8c0709b904ad35986f6991bb3080ead3c3c4a90c
David Basarab <dbasarab617@gmail.com>
2023-01-07 19:10:40 -0500

    created interface

---
+++ src/ToolKit/Web/Api/SignalR/GetUserClaim.cs
@@ -0,0 +1,26 @@
+﻿using FatCat.Toolkit.Console;
+
+namespace FatCat.Toolkit.Web.Api.SignalR;
+
+public interface IGetUserClaim
+{
+	/// <summary>
+	///  Return claim from the user if found
+	/// </summary>
+	/// <param name="user">ToolkitUser</param>
+	/// <param name="claimType">name of the claim</param>
+	/// <returns>Claim, null if claim is not found</returns>
+	ToolkitClaim? GetClaim(ToolkitUser user, string claimType);
+}
+
+public class GetUserClaim : IGetUserClaim
+{
+	public ToolkitClaim? GetClaim(ToolkitUser user, string claimType)
+	{
+		ConsoleLog.Write("Dog");
+		ConsoleLog.Write("Dog");
+		ConsoleLog.Write("Dog");
+
+		return null;
+	}
+}
