aeedf57affe01e1a92d05acb7c9760ba3c25834b
David Basarab <dbasarab617@gmail.com>
2023-02-11 11:48:03 -0500

    added static creat function for toolkit claim

--- src/ToolKit/Web/Api/SignalR/ToolkitClaim.cs
+++ src/ToolKit/Web/Api/SignalR/ToolkitClaim.cs
@@ -1,7 +1,17 @@
+using System.Security.Claims;
+
 namespace FatCat.Toolkit.Web.Api.SignalR;
 
 public class ToolkitClaim : EqualObject
 {
+	public static ToolkitClaim Create(Claim claim) => new ToolkitClaim
+													{
+														Issuer = claim.Issuer,
+														Type = claim.Type,
+														Value = claim.Value,
+														OriginalIssuer = claim.OriginalIssuer
+													};
+
 	public string Issuer { get; set; }
 
 	public string OriginalIssuer { get; set; }
