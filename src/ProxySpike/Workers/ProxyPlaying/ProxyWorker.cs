7abad06108ca4f1cf9b846dfdf8dd50ad8eea4b0
David Basarab <dbasarab617@gmail.com>
2023-02-23 13:58:03 -0500

    notes on proxy tests

--- src/ProxySpike/Workers/ProxyPlaying/ProxyWorker.cs
+++ src/ProxySpike/Workers/ProxyPlaying/ProxyWorker.cs
@@ -4,6 +4,14 @@ using ProxySpike.Options;
 
 namespace ProxySpike.Workers.ProxyPlaying;
 
+/*
+ *	What do we need to test to ensure we can use this as a proxy?
+ *		- Redirect of get, post, and delete
+ *		- Redirect of Meta Data to a different endpoint than everything else
+ *		- Can do we logic in the redirect to completely replace what NetworkManager is dong?
+ * 
+ */
+
 public class ProxyWorker : ISpikeWorker<ProxyOptions>
 {
 	public Task DoWork(ProxyOptions options)
