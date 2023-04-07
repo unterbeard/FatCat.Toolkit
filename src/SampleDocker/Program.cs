774d28b9a40f1a511e4f85fa5d7e787804cd7f10
David Basarab <dbasarab617@gmail.com>
2023-04-07 11:04:12 -0400

    added project to solution

--- src/SampleDocker/Program.cs
+++ src/SampleDocker/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FatCat.Toolkit.Console;
+
+namespace SampleDocker;
+
+public static class Program
+{
+	public static void Main(params string[] args) { ConsoleLog.Write("This will try to get the docker image working in the cloud"); }
+}
