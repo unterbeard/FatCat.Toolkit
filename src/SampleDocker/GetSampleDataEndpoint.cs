47e400771a04bb92c0306b7fbc71f76e2891057b
David Basarab <dbasarab617@gmail.com>
2023-04-08 13:57:03 -0400

    local setting working

---
+++ src/SampleDocker/GetSampleDataEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using FatCat.Toolkit;
+using FatCat.Toolkit.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleDocker;
+
+public class GetSampleDataEndpoint : Endpoint
+{
+	private readonly IConfiguration configuration;
+
+	public GetSampleDataEndpoint(IConfiguration configuration) => this.configuration = configuration;
+
+	[HttpGet("api/SampleData")]
+	public WebResult GetSampleData()
+	{
+		var response = new SampleResponse
+						{
+							ConfigValue = configuration["PlayingSetting"],
+							SomeMetaData = "Some Meta Data"
+						};
+
+		return Ok(response);
+	}
+}
+
+public class SampleResponse : EqualObject
+{
+	public string ConfigValue { get; set; }
+
+	public string SomeMetaData { get; set; }
+}
