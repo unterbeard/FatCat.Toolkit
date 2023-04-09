eb25c5e55c640056676aecb9aac810ec77a3ab7d
David Basarab <dbasarab617@gmail.com>
2023-04-08 20:23:45 -0400

    wip

---
+++ src/SampleDocker/GetStorageItemsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using FatCat.Toolkit.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleDocker;
+
+public class GetStorageItemsEndpoint : Endpoint
+{
+	[HttpPost("api/store")]
+	public WebResult GetStorageItems()
+	{
+		// TODO: Replace <storage-account-name> with your actual storage account name
+		var blobServiceClient = new BlobServiceClient(
+													new Uri("https://<storage-account-name>.blob.core.windows.net"),
+													new DefaultAzureCredential());
+
+		//Create a unique name for the container
+		string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+		// Create the container and return a container client object
+		BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+		
+		return NotImplemented();
+	}
+}
