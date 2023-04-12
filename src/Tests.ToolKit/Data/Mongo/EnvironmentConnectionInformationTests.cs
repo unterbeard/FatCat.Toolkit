75ccde78a61ed7fbbd299f945a7a83acab548e46
David Basarab <dbasarab617@gmail.com>
2023-04-11 21:34:19 -0400

    a new way to get mongo connection info

---
+++ src/Tests.ToolKit/Data/Mongo/EnvironmentConnectionInformationTests.cs
@@ -0,0 +1,81 @@
+﻿using FakeItEasy;
+using FatCat.Fakes;
+using FatCat.Toolkit.Data;
+using FatCat.Toolkit.Data.Mongo;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.FatCat.Toolkit.Data.Mongo;
+
+public class EnvironmentConnectionInformationTests
+{
+	private readonly EnvironmentConnectionInformation connectionInformation;
+	private string connectionString;
+	private string dataBaseName;
+	private IEnvironmentRepository environmentRepository;
+
+	public EnvironmentConnectionInformationTests()
+	{
+		SetUpEnvironmentRepository();
+
+		connectionInformation = new EnvironmentConnectionInformation(environmentRepository);
+	}
+
+	[Fact]
+	public void OnConnectionStringReadConnectionStringFromEnvironmentRepository()
+	{
+		connectionInformation.GetConnectionString();
+
+		A.CallTo(() => environmentRepository.Get("MongoConnectionString"))
+		.MustHaveHappenedOnceExactly();
+	}
+
+	[Fact]
+	public void OnDatabaseNameReadFromEnvironmentRepository()
+	{
+		connectionInformation.GetDatabaseName();
+
+		A.CallTo(() => environmentRepository.Get("MongoDatabaseName"))
+		.MustHaveHappenedOnceExactly();
+	}
+
+	[Fact]
+	public void ReturnConnectionStringFromRepository()
+	{
+		connectionInformation.GetConnectionString()
+							.Should()
+							.Be(connectionString);
+	}
+
+	[Fact]
+	public void ReturnDatabaseNameFromRepository()
+	{
+		connectionInformation.GetDatabaseName()
+							.Should()
+							.Be(dataBaseName);
+	}
+
+	private void SetUpEnvironmentRepository()
+	{
+		environmentRepository = A.Fake<IEnvironmentRepository>();
+
+		SetUpGetConnectionString();
+		SetUpGetDatabaseName();
+	}
+
+	private void SetUpGetConnectionString()
+	{
+		connectionString = Faker.RandomString();
+
+		A.CallTo(() => environmentRepository.Get("MongoConnectionString"))
+		.Returns(connectionString);
+	}
+
+	private void SetUpGetDatabaseName()
+	{
+		dataBaseName = Faker.RandomString();
+
+		A.CallTo(() => environmentRepository.Get("MongoDatabaseName"))
+		.Returns(dataBaseName);
+	}
+}
---
+++ src/ToolKit/Data/Mongo/EnvironmentConnectionInformation.cs
@@ -0,0 +1,12 @@
+﻿namespace FatCat.Toolkit.Data.Mongo;
+
+public class EnvironmentConnectionInformation : IMongoConnectionInformation
+{
+	private readonly IEnvironmentRepository environmentRepository;
+
+	public EnvironmentConnectionInformation(IEnvironmentRepository environmentRepository) => this.environmentRepository = environmentRepository;
+
+	public string GetConnectionString() => environmentRepository.Get("MongoConnectionString");
+
+	public string GetDatabaseName() => environmentRepository.Get("MongoDatabaseName");
+}
