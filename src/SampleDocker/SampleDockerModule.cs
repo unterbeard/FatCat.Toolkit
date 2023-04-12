bbe8c199be068ba6119e110a3eceb89b5d0ff95c
David Basarab <dbasarab617@gmail.com>
2023-04-11 21:44:01 -0400

    got mongo working from interface

---
+++ src/SampleDocker/SampleDockerModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using FatCat.Toolkit.Data.Mongo;
+
+namespace SampleDocker;
+
+public class SampleDockerModule : Module
+{
+	protected override void Load(ContainerBuilder builder)
+	{
+		builder.RegisterType<SampleConnectionInformation>()
+				.As<IMongoConnectionInformation>();
+	}
+}
