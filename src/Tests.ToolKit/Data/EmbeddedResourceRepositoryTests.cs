a7450ccfded133334fef08bcdbf5a31726a8f1be
David Basarab <dbasarab617@gmail.com>
2023-04-25 20:59:10 -0400

    added a way to read the names

---
+++ src/Tests.ToolKit/Data/EmbeddedResourceRepositoryTests.cs
@@ -0,0 +1,50 @@
+﻿using FatCat.Toolkit.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.FatCat.Toolkit.Data;
+
+public class EmbeddedResourceRepositoryTests
+{
+	private const string ValidResourceName = "Tests.FatCat.Toolkit.Data.ResourceItemToGet.txt";
+	private readonly EmbeddedResourceRepository repository;
+
+	public EmbeddedResourceRepositoryTests() => repository = new EmbeddedResourceRepository();
+
+	[Fact]
+	public void CanGetEmbeddedResourceText()
+	{
+		var text = repository.GetText(GetType().Assembly, ValidResourceName);
+
+		text
+			.Should()
+			.Be("Dreams find fulfillment in time");
+	}
+
+	[Fact]
+	public void IfResourceIsNotFoundReturnNull()
+	{
+		var resourceName = "Tests.FatCat.Toolkit.Data.ResourceItemToGetThatDoesNotExist.txt";
+
+		var text = repository.GetText(GetType().Assembly, resourceName);
+
+		text
+			.Should()
+			.BeNullOrEmpty();
+	}
+
+	[Fact]
+	public void ReturnAllResourceNames()
+	{
+		var resources = repository.GetAllResourceNames(GetType().Assembly);
+
+		resources
+			.Count
+			.Should()
+			.BeGreaterThanOrEqualTo(1);
+
+		resources
+			.Should()
+			.Contain(ValidResourceName);
+	}
+}
---
+++ src/ToolKit/Data/EmbeddedResourceRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace FatCat.Toolkit.Data;
+
+public interface IEmbeddedResourceRepository
+{
+	List<string> GetAllResourceNames(Assembly assembly);
+
+	string GetText(Assembly assembly, string resourceName);
+}
+
+public class EmbeddedResourceRepository : IEmbeddedResourceRepository
+{
+	public List<string> GetAllResourceNames(Assembly assembly) => assembly.GetManifestResourceNames().ToList();
+
+	public string GetText(Assembly assembly, string resourceName)
+	{
+		var manifestStream = assembly.GetManifestResourceStream(resourceName);
+
+		if (manifestStream is null) return null;
+
+		var streamReader = new StreamReader(manifestStream);
+
+		return streamReader.ReadToEnd();
+	}
+}
