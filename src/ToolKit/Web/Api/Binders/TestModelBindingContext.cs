dbd20b6485ab9d42fb18b8a9044efcb975850ad1
David Basarab <dbasarab617@gmail.com>
2023-05-29 16:43:51 -0400

    brough over BindContext testing helpers

---
+++ src/ToolKit/Web/Api/Binders/TestModelBindingContext.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace FatCat.Toolkit.Web.Api.Binders;
+
+public class TestModelBindingContext : ModelBindingContext
+{
+	public override ActionContext ActionContext { get; set; }
+
+	public override string BinderModelName { get; set; }
+
+	public override BindingSource BindingSource { get; set; }
+
+	public override string FieldName { get; set; }
+
+	public override bool IsTopLevelObject { get; set; }
+
+	public override object Model { get; set; }
+
+	public override ModelMetadata ModelMetadata { get; set; }
+
+	public override string ModelName { get; set; }
+
+	public override ModelStateDictionary ModelState { get; set; }
+
+	public override Func<ModelMetadata, bool> PropertyFilter { get; set; }
+
+	public override ModelBindingResult Result { get; set; }
+
+	public TestValueProvider TestValueProvider { get; set; }
+
+	public override ValidationStateDictionary ValidationState { get; set; }
+
+	public override IValueProvider ValueProvider
+	{
+		get => TestValueProvider;
+		set { }
+	}
+
+	public TestModelBindingContext(TestValueProvider testValueProvider) => TestValueProvider = testValueProvider;
+
+	public TestModelBindingContext() => TestValueProvider = new();
+
+	public void AddTestValues(string name, string value) => TestValueProvider.AddTestValues(name, value);
+
+	public override NestedScope EnterNestedScope(ModelMetadata modelMetadata, string fieldName, string modelName, object model) => throw new NotImplementedException();
+
+	public override NestedScope EnterNestedScope() => throw new NotImplementedException();
+
+	public void RemoveTestValues(string name) => TestValueProvider.RemoveTestValues(name);
+
+	protected override void ExitNestedScope() { throw new NotImplementedException(); }
+}
---
+++ src/ToolKit/Web/Api/Binders/TestValueProvider.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FatCat.Toolkit.Web.Api.Binders;
+
+public class TestValueProvider : IValueProvider
+{
+	private readonly Dictionary<string, string> values = new();
+
+	public void AddTestValues(string name, string value) => values.Add(name, value);
+
+	public bool ContainsPrefix(string prefix) => values.ContainsKey(prefix);
+
+	public ValueProviderResult GetValue(string key)
+	{
+		if (values.TryGetValue(key, out var value)) return new ValueProviderResult(value);
+
+		return ValueProviderResult.None;
+	}
+
+	public void RemoveTestValues(string name) => values.Remove(name);
+}
