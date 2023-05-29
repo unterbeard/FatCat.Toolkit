b131159daff7eca844fc07c6cc204ae2f1efbb55
David Basarab <dbasarab617@gmail.com>
2023-05-29 15:33:29 -0400

    added binders to toolkit

---
+++ src/ToolKit/Web/Api/Binders/IToolkitBinder.cs
@@ -0,0 +1,6 @@
+﻿namespace FatCat.Toolkit.Web.Api.Binders;
+
+public interface IToolkitBinder
+{
+	WebResult Result { get; set; }
+}
---
+++ src/ToolKit/Web/Api/Binders/ToolkitBinderFilterAttribute.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FatCat.Toolkit.Web.Api.Binders;
+
+[ExcludeFromCodeCoverage(Justification = "Helper too simple to test")]
+public class ToolkitBinderFilterAttribute : ActionFilterAttribute
+{
+	public override void OnActionExecuting(ActionExecutingContext context)
+	{
+		foreach (var value in context.ActionArguments.Values)
+		{
+			if (value is not IToolkitBinder binder) continue;
+
+			if (binder.Result.IsUnsuccessful) context.Result = binder.Result;
+
+			return;
+		}
+	}
+}
