6ba010c41aa9aaf470dcc168a20e1f50b8e31876
DBasarab <dbasarab617@gmail.com>
2023-08-30 14:44:55 -0400

    added in interfaces

--- src/ToolKit/DateTimeUtilities.cs
+++ src/ToolKit/DateTimeUtilities.cs
@@ -2,10 +2,14 @@ namespace FatCat.Toolkit;
 
 public interface IDateTimeUtilities
 {
+	DateTime LocalNow();
+
 	DateTime UtcNow();
 }
 
 public class DateTimeUtilities : IDateTimeUtilities
 {
+	public DateTime LocalNow() => DateTime.Now;
+
 	public DateTime UtcNow() => DateTime.UtcNow;
 }
