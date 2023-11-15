8fce164ac0762a44d23e41aa9b3afe0ed51ec2c8
David Basarab <dbasarab617@gmail.com>
2023-11-15 13:03:54 -0500

    cleaned up tool kit and removed stuff

--- src/ToolKit/Data/Lite/LiteDbConnectionException.cs
+++
@@ -1,7 +0,0 @@
-namespace FatCat.Toolkit.Data.Lite;
-
-public class LiteDbConnectionException : Exception
-{
-	public LiteDbConnectionException()
-		: base("You must connect to LiteDb") { }
-}
--- src/ToolKit/Data/Lite/LiteDbObject.cs
+++
@@ -1,6 +0,0 @@
-namespace FatCat.Toolkit.Data.Lite;
-
-public class LiteDbObject : DataObject
-{
-	public int Id { get; set; }
-}
