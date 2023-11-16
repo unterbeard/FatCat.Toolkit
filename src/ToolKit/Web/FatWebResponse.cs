c7b45689a1b80d334db01ad6e102ab3e035ef3a5
David Basarab <dbasarab617@gmail.com>
2023-11-16 10:02:37 -0500

    added more helper methods

--- src/ToolKit/Web/FatWebResponse.cs
+++ src/ToolKit/Web/FatWebResponse.cs
@@ -1,28 +1,52 @@
 ﻿using System.Net;
+using Newtonsoft.Json;
 
 namespace FatCat.Toolkit.Web;
 
 public class FatWebResponse<T> : EqualObject
 	where T : class
 {
+	public static FatWebResponse<T> Ok(T data) => new(HttpStatusCode.OK, data);
+
 	public FatWebResponse BaseResponse { get; }
 
-	public string Content => BaseResponse.Content;
+	public string Content
+	{
+		get => BaseResponse.Content;
+	}
 
-	public string ContentType => BaseResponse.ContentType;
+	public string ContentType
+	{
+		get => BaseResponse.ContentType;
+	}
 
-	public T Data => BaseResponse.IsSuccessful ? BaseResponse.To<T>() : null;
+	public T Data
+	{
+		get => BaseResponse.IsSuccessful ? BaseResponse.To<T>() : null;
+	}
 
-	public bool IsSuccessful => BaseResponse.IsSuccessful;
+	public bool IsSuccessful
+	{
+		get => BaseResponse.IsSuccessful;
+	}
 
-	public bool IsUnsuccessful => !IsSuccessful;
+	public bool IsUnsuccessful
+	{
+		get => !IsSuccessful;
+	}
 
-	public HttpStatusCode StatusCode => BaseResponse.StatusCode;
+	public HttpStatusCode StatusCode
+	{
+		get => BaseResponse.StatusCode;
+	}
 
 	public FatWebResponse(HttpStatusCode statusCode) =>
 		BaseResponse = new FatWebResponse { StatusCode = statusCode };
 
 	public FatWebResponse(FatWebResponse webResponse) => BaseResponse = webResponse;
+
+	public FatWebResponse(HttpStatusCode statusCode, T data) =>
+		BaseResponse = new FatWebResponse { StatusCode = statusCode, Content = JsonConvert.SerializeObject(data) };
 
 	public override string ToString() =>
 		$"FatWebResponse | StatusCode <{StatusCode}> | Type {typeof(T).FullName} | {Content}";
@@ -49,9 +73,15 @@ public class FatWebResponse : EqualObject
 
 	public string ContentType { get; set; } = "application/json; charset=UTF-8";
 
-	public bool IsSuccessful => (int)StatusCode >= 200 && (int)StatusCode <= 299;
+	public bool IsSuccessful
+	{
+		get => (int)StatusCode >= 200 && (int)StatusCode <= 299;
+	}
 
-	public bool IsUnsuccessful => !IsSuccessful;
+	public bool IsUnsuccessful
+	{
+		get => !IsSuccessful;
+	}
 
 	public HttpStatusCode StatusCode { get; set; }
 
