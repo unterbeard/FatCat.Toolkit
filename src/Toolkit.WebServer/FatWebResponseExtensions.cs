c8e26068f435fa95d9e9193887d5dc37e30dbf2d
David Basarab <dbasarab617@gmail.com>
2023-11-16 09:21:03 -0500

    added a way to create WebREsult from response

---
+++ src/Toolkit.WebServer/FatWebResponseExtensions.cs
@@ -0,0 +1,8 @@
+﻿using FatCat.Toolkit.Web;
+
+namespace FatCat.Toolkit.WebServer;
+
+public static class FatWebResponseExtensions
+{
+	public static WebResult ToWebResult(this FatWebResponse response) => new(response);
+}
