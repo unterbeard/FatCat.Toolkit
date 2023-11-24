f0de366c5b4019bca35f043f1348af88e4b3763c
David Basarab <dbasarab617@gmail.com>
2023-11-24 15:27:46 -0500

    Stepped project to version 1.0.185

--- src/ToolKit/FatResult.cs
+++ src/ToolKit/FatResult.cs
@@ -2,6 +2,16 @@
 
 public class FatResult<T> : EqualObject
 {
+	public static FatResult<T> Failed(string message)
+	{
+		return new FatResult<T> { IsSuccessful = false, Message = message };
+	}
+
+	public static FatResult<T> Success(T data)
+	{
+		return new FatResult<T> { IsSuccessful = true, Data = data };
+	}
+
 	public T Data { get; set; }
 
 	public bool IsSuccessful { get; set; }
