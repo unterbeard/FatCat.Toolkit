f95c0d837a6819dbad756529dcfddcf54e45c6b4
David Basarab <dbasarab617@gmail.com>
2023-12-31 22:06:37 -0500

    added back encoded methods

--- src/Tests.ToolKit/ByteToolTests.cs
+++ src/Tests.ToolKit/ByteToolTests.cs
@@ -20,4 +20,18 @@ public class ByteToolTests
 
 		decodedBytes.Should().BeEquivalentTo(bytes);
 	}
+
+	[Fact]
+	public void CanConvertToByteArrayAndFromEncoded()
+	{
+		var byteTools = new ByteTools();
+
+		var textToChange = Faker.RandomString();
+
+		var bytes = byteTools.FromBase64Encoded(textToChange);
+
+		var plainText = byteTools.ToBase64Encoded(bytes);
+
+		plainText.Should().Be(textToChange);
+	}
 }
--- src/ToolKit/ByteTools.cs
+++ src/ToolKit/ByteTools.cs
@@ -1,17 +1,52 @@
-﻿namespace FatCat.Toolkit;
+﻿using System.Text;
+using FatCat.Toolkit.Extensions;
+
+namespace FatCat.Toolkit;
 
 public interface IByteTools
 {
+	byte[] FromBase64Encoded(string text);
+
 	byte[] FromBase64String(string text);
+
+	string ToBase64Encoded(byte[] bytes);
 
 	string ToBase64String(byte[] bytes);
 }
 
 public class ByteTools : IByteTools
 {
+	public byte[] FromBase64Encoded(string text)
+	{
+		if (text.IsNullOrEmpty())
+		{
+			return Array.Empty<byte>();
+		}
+
+		var plainTextBytes = Encoding.UTF8.GetBytes(text);
+
+		var base64Text = Convert.ToBase64String(plainTextBytes);
+
+		return Convert.FromBase64String(base64Text);
+	}
+
 	public byte[] FromBase64String(string text)
 	{
 		return Convert.FromBase64String(text);
+	}
+
+	public string ToBase64Encoded(byte[] bytes)
+	{
+		if (bytes.Length == 0)
+		{
+			return string.Empty;
+		}
+
+		var base64Text = Convert.ToBase64String(bytes);
+
+		var plainTextBytes = Convert.FromBase64String(base64Text);
+
+		return Encoding.UTF8.GetString(plainTextBytes);
 	}
 
 	public string ToBase64String(byte[] bytes)
