55d8915bd2678f522a4bb9bd2a63d3e446136056
David Basarab <dbasarab617@gmail.com>
2024-01-29 20:25:16 -0500

    added cat bounce

---
+++ src/ToolKit/Debouncing/CatBounce.cs
@@ -0,0 +1,33 @@
+﻿using DebounceThrottle;
+
+namespace FatCat.Toolkit.Debouncing;
+
+public interface ICatBounce
+{
+	void Debounce(Action action);
+
+	void Throttle(Action action);
+
+	void ThrottleAsync(Func<Task> action);
+}
+
+public class CatBounce(TimeSpan interval) : ICatBounce
+{
+	private readonly DebounceDispatcher debounceDispatcher = new((int)interval.TotalMilliseconds);
+	private readonly ThrottleDispatcher throttleDispatcher = new((int)interval.TotalMilliseconds);
+
+	public void Debounce(Action action)
+	{
+		debounceDispatcher.Debounce(action);
+	}
+
+	public void Throttle(Action action)
+	{
+		throttleDispatcher.Throttle(action);
+	}
+
+	public void ThrottleAsync(Func<Task> action)
+	{
+		throttleDispatcher.ThrottleAsync(action).Wait();
+	}
+}
---
+++ src/ToolKit/Debouncing/CatBounceFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace FatCat.Toolkit.Debouncing;
+
+public interface ICatBounceFactory
+{
+	ICatBounce Create(TimeSpan interval);
+}
+
+public class CatBounceFactory : ICatBounceFactory
+{
+	public ICatBounce Create(TimeSpan interval)
+	{
+		return new CatBounce(interval);
+	}
+}
