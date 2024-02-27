83f7fedb7089b7f270649a004bab960856ae0ab0
David Basarab <dbasarab617@gmail.com>
2024-02-26 20:48:33 -0500

    got encryption working

--- src/ToolKit/Cryptography/FatCatAesEncryption.cs
+++ src/ToolKit/Cryptography/FatCatAesEncryption.cs
@@ -13,52 +13,39 @@ public class FatCatAesEncryption : IFatCatAesEncryption
 {
 	public byte[] Decrypt(byte[] cypherData, byte[] key, byte[] iv)
 	{
-		using (var aesAlg = Aes.Create())
-		{
-			aesAlg.Key = key;
-			aesAlg.IV = iv;
-			var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-			byte[] decryptedBytes;
+		using var aesAlg = Aes.Create();
 
-			using (var msDecrypt = new MemoryStream(cypherData))
-			{
-				using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-				{
-					using (var msPlain = new MemoryStream())
-					{
-						csDecrypt.CopyTo(msPlain);
-						decryptedBytes = msPlain.ToArray();
-					}
-				}
-			}
+		aesAlg.Key = key;
+		aesAlg.IV = iv;
+		var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-			// return Encoding.UTF8.GetString(decryptedBytes);
+		using var msDecrypt = new MemoryStream(cypherData);
 
-			return decryptedBytes;
-		}
+		using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+
+		using var msPlain = new MemoryStream();
+
+		csDecrypt.CopyTo(msPlain);
+
+		return msPlain.ToArray();
 	}
 
 	public byte[] Encrypt(byte[] openData, byte[] key, byte[] iv)
 	{
-		using (var aesAlg = Aes.Create())
+		using var aesAlg = Aes.Create();
+
+		aesAlg.Key = key;
+		aesAlg.IV = iv;
+
+		var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+		using var msEncrypt = new MemoryStream();
+
+		using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
 		{
-			aesAlg.Key = key;
-			aesAlg.IV = iv;
-			var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-			byte[] encryptedBytes;
-
-			using (var msEncrypt = new MemoryStream())
-			{
-				using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-				{
-					// byte[] plainBytes = Encoding.UTF8.GetBytes(plaintext);
-					csEncrypt.Write(openData, 0, openData.Length);
-				}
-
-				encryptedBytes = msEncrypt.ToArray();
-			}
-
-			return encryptedBytes;
+			csEncrypt.Write(openData, 0, openData.Length);
 		}
+
+		return msEncrypt.ToArray();
 	}
 }
