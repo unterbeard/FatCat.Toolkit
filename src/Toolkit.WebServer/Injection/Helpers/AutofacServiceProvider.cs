e500434792e7f1f396a284635ac9acb1559c774f
David Basarab <dbasarab617@gmail.com>
2024-04-14 14:04:29 -0400

    working now

--- src/Toolkit.WebServer/Injection/Helpers/AutofacServiceProvider.cs
+++ src/Toolkit.WebServer/Injection/Helpers/AutofacServiceProvider.cs
@@ -1,217 +1,141 @@
-﻿// Copyright (c) Autofac Project. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2015 Autofac Contributors
+// https://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
+using System.Diagnostics.CodeAnalysis;
 using Autofac;
-using Autofac.Core;
 using Microsoft.Extensions.DependencyInjection;
-using KeyedService = Autofac.Core.KeyedService;
 
-namespace FatCat.Toolkit.WebServer.Injection.Helpers;
-
-/// <summary>
-/// Autofac implementation of the ASP.NET Core <see cref="IServiceProvider"/>.
-/// </summary>
-/// <seealso cref="IServiceProvider" />
-/// <seealso cref="ISupportRequiredService" />
-public partial class AutofacServiceProvider
-	: IServiceProvider,
-		ISupportRequiredService,
-		IKeyedServiceProvider,
-		IServiceProviderIsService,
-		IServiceProviderIsKeyedService,
-		IDisposable,
-		IAsyncDisposable
+namespace FatCat.Toolkit.WebServer.Injection.Helpers
 {
-	private readonly ILifetimeScope _lifetimeScope;
-
-	private bool _disposed;
-
 	/// <summary>
-	/// Initializes a new instance of the <see cref="AutofacServiceProvider"/> class.
+	/// Autofac implementation of the ASP.NET Core <see cref="IServiceProvider"/>.
 	/// </summary>
-	/// <param name="lifetimeScope">
-	/// The lifetime scope from which services will be resolved.
-	/// </param>
-	public AutofacServiceProvider(ILifetimeScope lifetimeScope)
+	/// <seealso cref="System.IServiceProvider" />
+	/// <seealso cref="Microsoft.Extensions.DependencyInjection.ISupportRequiredService" />
+	public class AutofacServiceProvider : IServiceProvider, ISupportRequiredService, IDisposable, IAsyncDisposable
 	{
-		_lifetimeScope = lifetimeScope;
-	}
+		private readonly ILifetimeScope _lifetimeScope;
 
-	/// <summary>
-	/// Gets the service object of the specified type.
-	/// </summary>
-	/// <param name="serviceType">
-	/// An object that specifies the type of service object to get.
-	/// </param>
-	/// <param name="serviceKey">
-	/// An object that specifies the key of service object to get.
-	/// </param>
-	/// <returns>
-	/// A service object of type <paramref name="serviceType" />; or <see langword="null" />
-	/// if there is no service object of type <paramref name="serviceType" />.
-	/// </returns>
-	public object GetKeyedService(Type serviceType, object serviceKey)
-	{
-		if (serviceKey is null)
-		{
-			// A null key equates to "not keyed."
-			return _lifetimeScope.ResolveOptional(serviceType);
-		}
-		else
-		{
-			try
-			{
-				return _lifetimeScope.ResolveOptionalService(new KeyedService(serviceKey, serviceType));
-			}
-			catch (DependencyResolutionException ex)
-				when (ex.InnerException is KeyTypeConversionException conversionException)
-			{
-				// If the issue was with converting the specified key type to
-				// match a [ServiceKey] parameter type, the M.E.DI contract is
-				// that it must be an InvalidOperationException.
-				throw new InvalidOperationException(conversionException.Message, conversionException);
-			}
-		}
-	}
+		private bool _disposed = false;
 
-	/// <summary>
-	/// Gets service of type <paramref name="serviceType" /> from the
-	/// <see cref="AutofacServiceProvider" /> and requires it be present.
-	/// </summary>
-	/// <param name="serviceType">
-	/// An object that specifies the type of service object to get.
-	/// </param>
-	/// <param name="serviceKey">
-	/// An object that specifies the key of service object to get.
-	/// </param>
-	/// <returns>
-	/// A service object of type <paramref name="serviceType" />.
-	/// </returns>
-	/// <exception cref="Autofac.Core.Registration.ComponentNotRegisteredException">
-	/// Thrown if the <paramref name="serviceType" /> isn't registered with the container.
-	/// </exception>
-	/// <exception cref="Autofac.Core.DependencyResolutionException">
-	/// Thrown if the object can't be resolved from the container.
-	/// </exception>
-	public object GetRequiredKeyedService(Type serviceType, object serviceKey)
-	{
-		if (serviceKey is null)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AutofacServiceProvider"/> class.
+		/// </summary>
+		/// <param name="lifetimeScope">
+		/// The lifetime scope from which services will be resolved.
+		/// </param>
+		public AutofacServiceProvider(ILifetimeScope lifetimeScope)
 		{
-			// A null key equates to "not keyed."
-			return _lifetimeScope.Resolve(serviceType);
+			this._lifetimeScope = lifetimeScope;
 		}
-		else
+
+		/// <summary>
+		/// Gets service of type <paramref name="serviceType" /> from the
+		/// <see cref="AutofacServiceProvider" /> and requires it be present.
+		/// </summary>
+		/// <param name="serviceType">
+		/// An object that specifies the type of service object to get.
+		/// </param>
+		/// <returns>
+		/// A service object of type <paramref name="serviceType" />.
+		/// </returns>
+		/// <exception cref="Autofac.Core.Registration.ComponentNotRegisteredException">
+		/// Thrown if the <paramref name="serviceType" /> isn't registered with the container.
+		/// </exception>
+		/// <exception cref="Autofac.Core.DependencyResolutionException">
+		/// Thrown if the object can't be resolved from the container.
+		/// </exception>
+		public object GetRequiredService(Type serviceType)
 		{
-			try
+			return this._lifetimeScope.Resolve(serviceType);
+		}
+
+		/// <summary>
+		/// Gets the service object of the specified type.
+		/// </summary>
+		/// <param name="serviceType">
+		/// An object that specifies the type of service object to get.
+		/// </param>
+		/// <returns>
+		/// A service object of type <paramref name="serviceType" />; or <see langword="null" />
+		/// if there is no service object of type <paramref name="serviceType" />.
+		/// </returns>
+		public object GetService(Type serviceType)
+		{
+			return this._lifetimeScope.ResolveOptional(serviceType);
+		}
+
+		/// <summary>
+		/// Gets the underlying instance of <see cref="ILifetimeScope" />.
+		/// </summary>
+		public ILifetimeScope LifetimeScope => _lifetimeScope;
+
+		/// <summary>
+		/// Releases unmanaged and - optionally - managed resources.
+		/// </summary>
+		/// <param name="disposing">
+		/// <see langword="true" /> to release both managed and unmanaged resources;
+		/// <see langword="false" /> to release only unmanaged resources.
+		/// </param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this._disposed)
 			{
-				return _lifetimeScope.ResolveKeyed(serviceKey, serviceType);
-			}
-			catch (DependencyResolutionException ex)
-				when (ex.InnerException is KeyTypeConversionException conversionException)
-			{
-				// If the issue was with converting the specified key type to
-				// match a [ServiceKey] parameter type, the M.E.DI contract is
-				// that it must be an InvalidOperationException.
-				throw new InvalidOperationException(conversionException.Message, conversionException);
+				this._disposed = true;
+				if (disposing)
+				{
+					this._lifetimeScope.Dispose();
+				}
 			}
 		}
-	}
 
-	/// <summary>
-	/// Gets service of type <paramref name="serviceType" /> from the
-	/// <see cref="AutofacServiceProvider" /> and requires it be present.
-	/// </summary>
-	/// <param name="serviceType">
-	/// An object that specifies the type of service object to get.
-	/// </param>
-	/// <returns>
-	/// A service object of type <paramref name="serviceType" />.
-	/// </returns>
-	/// <exception cref="Autofac.Core.Registration.ComponentNotRegisteredException">
-	/// Thrown if the <paramref name="serviceType" /> isn't registered with the container.
-	/// </exception>
-	/// <exception cref="Autofac.Core.DependencyResolutionException">
-	/// Thrown if the object can't be resolved from the container.
-	/// </exception>
-	public object GetRequiredService(Type serviceType)
-	{
-		return _lifetimeScope.Resolve(serviceType);
-	}
-
-	/// <inheritdoc />
-	public bool IsKeyedService(Type serviceType, object serviceKey)
-	{
-		// Null service key means non-keyed.
-		if (serviceKey == null)
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
 		{
-			return IsService(serviceType);
-		}
-
-		return _lifetimeScope.ComponentRegistry.IsRegistered(new KeyedService(serviceKey, serviceType));
-	}
-
-	/// <inheritdoc />
-	public bool IsService(Type serviceType) =>
-		_lifetimeScope.ComponentRegistry.IsRegistered(new TypedService(serviceType));
-
-	/// <summary>
-	/// Gets the service object of the specified type.
-	/// </summary>
-	/// <param name="serviceType">
-	/// An object that specifies the type of service object to get.
-	/// </param>
-	/// <returns>
-	/// A service object of type <paramref name="serviceType" />; or <see langword="null" />
-	/// if there is no service object of type <paramref name="serviceType" />.
-	/// </returns>
-	public object GetService(Type serviceType)
-	{
-		return _lifetimeScope.ResolveOptional(serviceType);
-	}
-
-	/// <summary>
-	/// Gets the underlying instance of <see cref="ILifetimeScope" />.
-	/// </summary>
-	public ILifetimeScope LifetimeScope => _lifetimeScope;
-
-	/// <summary>
-	/// Releases unmanaged and - optionally - managed resources.
-	/// </summary>
-	/// <param name="disposing">
-	/// <see langword="true" /> to release both managed and unmanaged resources;
-	/// <see langword="false" /> to release only unmanaged resources.
-	/// </param>
-	protected virtual void Dispose(bool disposing)
-	{
-		if (!_disposed)
-		{
-			_disposed = true;
-			if (disposing)
-			{
-				_lifetimeScope.Dispose();
-			}
-		}
-	}
-
-	/// <summary>
-	/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-	/// </summary>
-	public void Dispose()
-	{
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
-
-	/// <summary>
-	/// Performs a dispose operation asynchronously.
-	/// </summary>
-	public async ValueTask DisposeAsync()
-	{
-		if (!_disposed)
-		{
-			_disposed = true;
-			await _lifetimeScope.DisposeAsync().ConfigureAwait(false);
+			this.Dispose(true);
 			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Performs a dispose operation asynchronously.
+		/// </summary>
+		[SuppressMessage(
+			"Usage",
+			"CA1816:Dispose methods should call SuppressFinalize",
+			Justification = "DisposeAsync should also call SuppressFinalize (see various .NET internal implementations)."
+		)]
+		public async ValueTask DisposeAsync()
+		{
+			if (!this._disposed)
+			{
+				this._disposed = true;
+				await this._lifetimeScope.DisposeAsync();
+				GC.SuppressFinalize(this);
+			}
 		}
 	}
 }
--- src/Toolkit.WebServer/Injection/Helpers/AutofacServiceScope.cs
+++ src/Toolkit.WebServer/Injection/Helpers/AutofacServiceScope.cs
@@ -1,77 +1,99 @@
-﻿// Copyright (c) Autofac Project. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2015 Autofac Contributors
+// https://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
 using Autofac;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FatCat.Toolkit.WebServer.Injection.Helpers;
-
-/// <summary>
-/// Autofac implementation of the ASP.NET Core <see cref="IServiceScope"/>.
-/// </summary>
-/// <seealso cref="IServiceScope" />
-internal class AutofacServiceScope : IServiceScope, IAsyncDisposable
+namespace FatCat.Toolkit.WebServer.Injection.Helpers
 {
-	private bool _disposed;
-	private readonly AutofacServiceProvider _serviceProvider;
-
 	/// <summary>
-	/// Initializes a new instance of the <see cref="AutofacServiceScope"/> class.
+	/// Autofac implementation of the ASP.NET Core <see cref="IServiceScope"/>.
 	/// </summary>
-	/// <param name="lifetimeScope">
-	/// The lifetime scope from which services should be resolved for this service scope.
-	/// </param>
-	public AutofacServiceScope(ILifetimeScope lifetimeScope)
+	/// <seealso cref="Microsoft.Extensions.DependencyInjection.IServiceScope" />
+	internal class AutofacServiceScope : IServiceScope, IAsyncDisposable
 	{
-		_serviceProvider = new AutofacServiceProvider(lifetimeScope);
-	}
+		private bool _disposed;
+		private readonly AutofacServiceProvider _serviceProvider;
 
-	/// <summary>
-	/// Gets an <see cref="IServiceProvider" /> corresponding to this service scope.
-	/// </summary>
-	/// <value>
-	/// An <see cref="IServiceProvider" /> that can be used to resolve dependencies from the scope.
-	/// </value>
-	public IServiceProvider ServiceProvider
-	{
-		get { return _serviceProvider; }
-	}
-
-	/// <summary>
-	/// Disposes of the lifetime scope and resolved disposable services.
-	/// </summary>
-	public void Dispose()
-	{
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
-
-	/// <summary>
-	/// Releases unmanaged and - optionally - managed resources.
-	/// </summary>
-	/// <param name="disposing">
-	/// <see langword="true" /> to release both managed and unmanaged resources; <see langword="false" /> to release only unmanaged resources.
-	/// </param>
-	protected virtual void Dispose(bool disposing)
-	{
-		if (!_disposed)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AutofacServiceScope"/> class.
+		/// </summary>
+		/// <param name="lifetimeScope">
+		/// The lifetime scope from which services should be resolved for this service scope.
+		/// </param>
+		public AutofacServiceScope(ILifetimeScope lifetimeScope)
 		{
-			_disposed = true;
+			this._serviceProvider = new AutofacServiceProvider(lifetimeScope);
+		}
 
-			if (disposing)
+		/// <summary>
+		/// Gets an <see cref="IServiceProvider" /> corresponding to this service scope.
+		/// </summary>
+		/// <value>
+		/// An <see cref="IServiceProvider" /> that can be used to resolve dependencies from the scope.
+		/// </value>
+		public IServiceProvider ServiceProvider
+		{
+			get { return this._serviceProvider; }
+		}
+
+		/// <summary>
+		/// Disposes of the lifetime scope and resolved disposable services.
+		/// </summary>
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Releases unmanaged and - optionally - managed resources.
+		/// </summary>
+		/// <param name="disposing">
+		/// <see langword="true" /> to release both managed and unmanaged resources; <see langword="false" /> to release only unmanaged resources.
+		/// </param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this._disposed)
 			{
-				_serviceProvider.Dispose();
+				this._disposed = true;
+
+				if (disposing)
+				{
+					this._serviceProvider.Dispose();
+				}
 			}
 		}
-	}
 
-	/// <inheritdoc/>
-	public async ValueTask DisposeAsync()
-	{
-		if (!_disposed)
+		public async ValueTask DisposeAsync()
 		{
-			_disposed = true;
-			await _serviceProvider.DisposeAsync().ConfigureAwait(false);
+			if (!this._disposed)
+			{
+				this._disposed = true;
+				await this._serviceProvider.DisposeAsync();
+			}
 		}
 	}
 }
