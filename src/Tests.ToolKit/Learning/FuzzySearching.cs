423a010c2fbe9773a14b90a72a92ddf5014b06d7
David Basarab <dbasarab617@gmail.com>
2024-06-19 20:22:44 -0400

    added fuzzy search extension

--- src/Tests.ToolKit/Learning/FuzzySearching.cs
+++ src/Tests.ToolKit/Learning/FuzzySearching.cs
@@ -1,23 +1,7 @@
 using FatCat.Toolkit;
+using FatCat.Toolkit.Extensions;
 
 namespace Tests.FatCat.Toolkit.Learning;
-
-public static class FuzzySearchingExtensions
-{
-	public static List<T> FuzzySearch<T>(this List<T> list, string search, Func<T, string> searchProperty)
-	{
-		var foundItems = new List<T>();
-
-		foreach (var item in list)
-		{
-			var propertyValue = searchProperty(item);
-
-			if (propertyValue.Contains(search, StringComparison.OrdinalIgnoreCase)) { foundItems.Add(item); }
-		}
-
-		return foundItems;
-	}
-}
 
 public class FuzzySearching
 {
